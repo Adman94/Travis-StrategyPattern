f06da644cfb5d492f38200de41db863fadfd366d
Adman94 <adman00794@gmail.com>
2018-12-04 19:14:40 -0800

    commit 3

--- hello/src/Travis/Hello.java
+++ hello/src/Travis/Hello.java
@@ -3,11 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hello;
+package Travis;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 /**
  *
- * @author habe
+ * @author adeeb
  */
 public class Hello {
 
@@ -15,7 +20,30 @@ public class Hello {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello");
+        List<String> aList = Arrays.asList("x", "y", "z"); 
+        List<Integer> nList = new ArrayList<>();
+        nList.add(7); 
+        nList.add(12); 
+        nList.add(44); 
+        
+        Predicate<Integer> p1 = num->num.equals(15); 
+        linearSearch(nList,p1); 
+        
+        Predicate<Integer> p2 = num->num.equals(44);
+        linearSearch(nList,p2); 
+        
+        
+        Predicate<String> ps = ch->ch.equals("y");
+        linearSearch(aList, ps);
+        
     }
-    
+ public static <T> int linearSearch(List<T> list, Predicate<T> check)
+ {
+     for(int i = 0; i< list.size(); i++) {
+        if(check.test(list.get(i)))
+            return i;
+     }
+      return -1; 
+    }
 }
